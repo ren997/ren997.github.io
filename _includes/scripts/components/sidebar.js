a3ebc472a952a130c01c611d10e869c8ebf95fe2
renjunjie <820812059@qq.com>
2026-03-06 16:10:21 +0800

    fix: 侧边栏-侧边栏增加收缩功能

--- _includes/scripts/components/sidebar.js
+++ _includes/scripts/components/sidebar.js
@@ -6,6 +6,9 @@
     var $pageRoot = $('.js-page-root');
     var $sidebarShow = $('.js-sidebar-show');
     var $sidebarHide = $('.js-sidebar-hide');
+    var $sidebarToggleDesktop = $('.js-sidebar-toggle-desktop');
+    var $sidebarExpandDesktop = $('.js-sidebar-expand-desktop');
+    var $tocToggles = $('.js-toc-toggle');
 
     function freeze(e) {
       if (e.target === $pageMask[0]) {
@@ -19,12 +22,34 @@
         window.removeEventListener('touchmove', freeze, { passive: false });
       }
     }
+    function syncDesktopSidebar() {
+      var collapsed = $pageRoot.hasClass('is-sidebar-collapsed');
+      $sidebarExpandDesktop.toggleClass('is-visible', collapsed);
+    }
 
     $sidebarShow.on('click', function() {
       stopBodyScrolling(true); $pageRoot.addClass('show-sidebar');
     });
     $sidebarHide.on('click', function() {
       stopBodyScrolling(false); $pageRoot.removeClass('show-sidebar');
+    });
+    $sidebarToggleDesktop.on('click', function() {
+      $pageRoot.addClass('is-sidebar-collapsed');
+      syncDesktopSidebar();
+    });
+    $sidebarExpandDesktop.on('click', function() {
+      $pageRoot.removeClass('is-sidebar-collapsed');
+      syncDesktopSidebar();
+    });
+    syncDesktopSidebar();
+
+    $tocToggles.on('click', function() {
+      var $toggle = $(this);
+      var $group = $toggle.closest('.toc-group');
+      var expanded = $group.hasClass('is-expanded');
+
+      $group.toggleClass('is-expanded', !expanded);
+      $toggle.attr('aria-expanded', expanded ? 'false' : 'true');
     });
   });
 })();
